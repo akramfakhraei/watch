f9c473c0859b20fbaeeaaf29d86a3692559cc5b3
akramfakhraei <arezoofakhraei74@gmail.com>
2024-02-28 16:47:19 +0330

    create sendOTP Handler

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+"use client";
+import { useState } from "react";
+import SendOTPFrom from "./SendOTPFrom";
+import http from "@/services/httpService";
 function AuthPage() {
+  const [phoneNumber, setPhoneNumber]=useState("");
+  console.log(phoneNumber);
+  const phoneNumberHandler=(e)=>{
+
+  };
+  const sendOTPHandler= async(e)=>{
+    e.preventDefault();
+    try {
+      const data = await http.post("/user/get-otp",{phoneNumber})
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  };
   return (
-    <div>AuthPage</div>
-  )
+    <div className="flex justify-center">
+      <div className="w-full sm:max-w-sm">
+        <SendOTPFrom
+        phoneNumber={phoneNumber}
+        onChange={phoneNumberHandler}
+        onSubmit={sendOTPHandler}/>
+      </div>
+    </div>
+  );
 }
 
 export default AuthPage

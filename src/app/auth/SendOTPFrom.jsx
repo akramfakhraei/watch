c05906f2e5cea8bd7b4f7d418736efc4782179a3
akramfakhraei <arezoofakhraei74@gmail.com>
2024-02-28 16:44:28 +0330

    create sendOTP form

---
+++ src/app/auth/SendOTPFrom.jsx
@@ -0,0 +1,19 @@
+import TextField from "@/common/TextField";
+function SendOTPFrom({phoneNumber,onChange,onSubmit}) {
+  return (
+    <div>
+      <form className=" space-y-10" onSubmit={onSubmit}>
+        <TextField 
+        label="شماره موبایل"
+        name="phoneNumber"
+        value={phoneNumber}
+        onChange={onChange} />
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تایید
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default SendOTPFrom

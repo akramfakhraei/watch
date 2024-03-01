b58240dd0197bbec87777a4598954a0c0b496863
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-01 18:36:46 +0330

    import providers func in layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css'
 import vazirFont from "@/constants/localFonts";
 import Header from "./Header";
 import { Toaster } from 'react-hot-toast';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +15,10 @@ export default function RootLayout({ children }) {
       <body className={`${vazirFont.variable} font-sans`}>
         <Toaster />
         <Header />
-          <div className="container xl:max-w-screen-xl">{children}</div>
+          <div className="container xl:max-w-screen-xl">
+            <Providers>{children}</Providers>
+            
+            </div>
           </body>
     </html>
   )

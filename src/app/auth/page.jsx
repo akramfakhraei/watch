a5f9d1a413713a8e46c5982814ae807726e6ea69
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-02 13:39:54 +0330

    useMutation

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 import SendOTPFrom from "./SendOTPFrom";
 import http from "@/services/httpService";
 import { toast } from "react-hot-toast";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "@/services/authServices";
+
 function AuthPage() {
   const [phoneNumber, setPhoneNumber]=useState("");
+  const {data , error, isLoading , mutateAsync}=useMutation({mutationFn:getOtp});
   console.log(phoneNumber);
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
@@ -12,20 +16,22 @@ function AuthPage() {
   const sendOTPHandler= async(e)=>{
     e.preventDefault();
     try {
-      const { data } = await http.post("/user/get-otp",{ phoneNumber });
+      const data =await mutateAsync({phoneNumber});
       console.log(data.data);
     } catch (error) {
       toast.error(error.response?.data?.message);
      
     }
   };
+  console.log({data, error,isLoading});
   return (
     <div className="flex justify-center">
       <div className="w-full sm:max-w-sm">
         <SendOTPFrom
         phoneNumber={phoneNumber}
         onChange={phoneNumberHandler}
-        onSubmit={sendOTPHandler}/>
+        onSubmit={sendOTPHandler}
+        isLoading={isLoading}/>
       </div>
     </div>
   );

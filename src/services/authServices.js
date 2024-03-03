90d8ed05b07155c0460aac18b3d027bc2e86d1a3
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-03 18:57:30 +0330

    create checkOtp http

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,3 +3,7 @@ export function getOtp(phoneNumber){
     return http.post("/user/get-otp",{ phoneNumber });
 
 }
+export function checkOtp(phoneNumber , otp){
+    return http.post("/user/check-otp",{ phoneNumber ,otp });
+
+}

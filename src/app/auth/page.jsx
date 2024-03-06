5ddec02867cff07b1913208c773cefce567ec58f
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-06 19:16:45 +0330

    edit page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,40 +1,95 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SendOTPFrom from "./SendOTPFrom";
-import http from "@/services/httpService";
 import { toast } from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
-import { getOtp } from "@/services/authServices";
+import { getOtp, checkOtp } from "@/services/authServices";
+import CheckOTPForm from "./CheckOTPForm";
+import { useRouter } from "next/navigation";
 
+const RESEND_TIME = 90;
 function AuthPage() {
-  const [phoneNumber, setPhoneNumber]=useState("");
-  const {data , error, isLoading , mutateAsync}=useMutation({mutationFn:getOtp});
+  const [phoneNumber, setPhoneNumber] = useState("09123214477");
+  const [otp, setOtp] = useState("");
+  const [step, setStep] = useState(2);
+  const [time, setTime] = useState(RESEND_TIME);
+  const router=useRouter();
+  const { data: otpResponse, error, isLoading, mutateAsync: mutateGetOtp } = useMutation({ mutationFn: getOtp });
   console.log(phoneNumber);
+  const { mutateAsync: mutateCheckOtp ,isLoading:isCheckingOtp} = useMutation({ mutationFn: checkOtp });
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
-  };
-  const sendOTPHandler= async(e)=>{
+  }; 
+  const sendOtpHandler = async (e) => { 
     e.preventDefault();
     try {
-      const data =await mutateAsync({phoneNumber});
+      const data = await mutateGetOtp({ phoneNumber });
+      toast.success(data.message);
       console.log(data.data);
+      setStep(2);
+      setTime(RESEND_TIME);
+      setOtp("");
     } catch (error) {
-      toast.error(error.response?.data?.message);
-     
+      toast.error(error?.response?.data?.message);
     }
   };
-  console.log({data, error,isLoading});
+  const checkOtpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { message, user } = await mutateCheckOtp({ phoneNumber, otp });
+      toast.success(message);
+      if (user.isActive) {
+        router.push("/");
+      } else {
+        router.push("/complete-profile");
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+  useEffect(() => {
+    const timer = time > 0 && setInterval(() => setTime((t) => t - 1), 1000);
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [time]);
+
+
+  const renderSteps = () => {
+    switch (step) {
+      case 1:
+        return (
+          <SendOTPFrom
+            phoneNumber={phoneNumber}
+            onChange={phoneNumberHandler}
+            onSubmit={sendOtpHandler}
+            isLoading={isLoading}
+          />
+        );
+      case 2:
+        return (
+          <CheckOTPForm
+            onBack={() => setStep((s) => s - 1)}
+            otp={otp}
+            setOtp={setOtp}
+            onSubmit={checkOtpHandler}
+            time={time}
+            onResendOtp={sendOtpHandler}
+            otpResponse={otpResponse}
+            isCheckingOtp={isCheckingOtp}
+          />
+        );
+      default:
+        return null;
+    }};
+
   return (
     <div className="flex justify-center">
       <div className="w-full sm:max-w-sm">
-        <SendOTPFrom
-        phoneNumber={phoneNumber}
-        onChange={phoneNumberHandler}
-        onSubmit={sendOTPHandler}
-        isLoading={isLoading}/>
+        {renderSteps()}
       </div>
     </div>
   );
 }
 
-export default AuthPage
+export default AuthPage;

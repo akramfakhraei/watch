afb90936beb75b1f443203bed44a78e7d9724d05
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-06 19:19:11 +0330

    add completeprofile query

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,9 +1,14 @@
 import http from "./httpService";
-export function getOtp(phoneNumber){
-    return http.post("/user/get-otp",{ phoneNumber });
+export function getOtp(data) {
+    return http.post("/user/get-otp", data ).then(({ data }) => data.data);;
 
 }
-export function checkOtp(phoneNumber , otp){
-    return http.post("/user/check-otp",{ phoneNumber ,otp });
+export function checkOtp(data) {
+    return http.post("/user/check-otp",  data ).then(({ data }) => data.data);;
 
 }
+export function completeProfile(data) {
+    return http
+      .post("/user/complete-profile", data).then(({ data }) => data.data);
+  }
+  

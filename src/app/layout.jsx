b396df5571fe04817526323f8bde05c485776be2
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-07 13:44:55 +0330

    debug

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,13 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html  lang="fa" dir="rtl">
-      <body className={`${vazirFont.variable} font-sans`}>
+      <body suppressHydrationWarning={true} className={`${vazirFont.variable} font-sans`}>
+      <Providers>
         <Toaster />
         <Header />
           <div className="container xl:max-w-screen-xl">
-            <Providers>{children}</Providers>
-            
+            {children}
             </div>
+            </Providers>
           </body>
     </html>
   )

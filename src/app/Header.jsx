2ebd31228185c6c278374edcb3c3786de42a6c17
akramfakhraei <arezoofakhraei74@gmail.com>
2024-03-07 14:51:04 +0330

    get User Profile with costume hook

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,12 +1,11 @@
 "use client";
+import { useGetUser } from "@/hooks/useAuth";
 import { getUserProfile } from "@/services/authServices";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 function Header() {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["get-user"],
-    queryFn: getUserProfile
-  })
+  const { user, cart } = data || {};
+  const { data, error, isLoading } = useGetUser();
   return (
     <header
     className={`shadow-md mb-10 sticky top-0 transition-all duration-200 bg-white ${
@@ -16,13 +15,12 @@ function Header() {
       <nav>
         <ul className="flex items-center  justify-between py-2 container xl:max-w-screen-xl">
           <li>
-
             <Link className="block py-2" href="/">
               خانه
             </Link>
           </li>
           <li>
-            <Link className="block py-2" href="/">
+            <Link className="block py-2" href="/produts">
               محصولات
             </Link>
           </li>
@@ -37,16 +35,19 @@ function Header() {
             </Link>
           </li>
           <li>
-            <Link className="block py-2" href="/">
-              سبد خرید
+            <Link className="block py-2" href="/cart">
+              سبد خرید ({cart ? cart.payDetail.productIds.length : 0})
             </Link>
           </li>
-          <li>
-            <Link className="block py-2" href="/auth">
-              ورود
-            </Link>
-          </li>
-
+          {user ? (
+            <span>{user.name}</span>
+          ) : (
+            <li>
+              <Link className="block py-2" href="/auth">
+                ورود
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>

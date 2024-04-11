9b66b2d1033996a42fbd0df80174d97133b918fc
akramfakhraei <arezoofakhraei74@gmail.com>
2024-04-11 17:41:46 +0430

    add

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -11,11 +11,13 @@ export function completeProfile(data) {
     return http
       .post("/user/complete-profile", data).then(({ data }) => data.data);
   }
-export function getUserProfile() {
-    return http
-      .get("/user/profile", data).then(({ data }) => data.data);
-  }
-export function updateProfile(data) {
-    return http.patch("/user/update", data).then(({ data }) => data.data);
+  export function getUserProfile() {
+    return http.get("/user/profile").then(({ data }) => data.data);
   }
   
+  export function updateProfile(data) {
+    return http.patch("/user/update", data).then(({ data }) => data.data);
+  }
+  export function logout() {
+    return http.post("/user/logout");
+  }

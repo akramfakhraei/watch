8289e75715974456d604534364e775b060a98393
akramfakhraei <arezoofakhraei74@gmail.com>
2024-04-11 17:45:34 +0430

    add loading to profile

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -1,8 +1,12 @@
 "use client";
+import Loading from "@/common/Loading";
 import { useGetUser } from "@/hooks/useAuth";
 import { toLocalDateString } from "@/utils/toLocalDate";
+import Link from "next/link";
 function Profile() {
   const { data, isLoading } = useGetUser();
+  const { user} = data || {};
+  if (isLoading) return <Loading />;
   return (
     <div className="py-4">
       <h1 className="mb-4 text-xl">
@@ -13,7 +17,7 @@ function Profile() {
         <span> {toLocalDateString(user.createdAt)} </span>
       </p>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
